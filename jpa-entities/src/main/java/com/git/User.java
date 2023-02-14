d32c666816534357d2d3f8ec8528e19d5ce43cdb
ozzy ozbourne <khanosaid726@gmail.com>
2023-02-14 17:06:14 +0530

    a mutli project

---
+++ jpa-entities/src/main/java/com/git/User.java
@@ -0,0 +1,23 @@
+package com.git;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    private long id;
+    private String fName;
+    private String lName;
+    private String email;
+
+}
---
+++ web-app/src/main/java/org/example/UserController.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+import com.git.User;
+
+
+public class UserController {
+    User saveUser() {
+        return new User();
+    }
+}
